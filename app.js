654574464761e93e1967cda407977e6e1a3fbbd0
Brian Yang <brian@brianyang.me>
2015-11-16 20:14:48 -0500

    Connect to SQL

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var app = require('express')();
+var sql = require('mysql');
+
+var con = sql.createConnection({
+    host: 'serv.byang.io',
+    user: 'root',
+    password: 'goseawolves2015',
+    database: 'test'
+});
 
 app.get('/', function(err, req, res) {
+    con.query('SHOW TABLES', function(err, rows) {
+        console.log(rows);
+    });
     return 'Hello world';
 });
 
