5a2cf0c7affb745ff5861516d09b1aaf12c9ffd5
Brian Yang <brian@brianyang.me>
2015-11-18 18:27:21 -0500

    Add initial query listing auctions

---
+++ public/javascripts/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#submit-button').on('click', function() {
+        console.warn("Submit!");
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+    host: "serv.byang.io",
+    user: "root",
+    password: "goseawolves2015",
+    database: 'test'
+});
+
+/* Get home page */
+router.get('/', function (req, res, next) {
+
+    // get list of auctions and pass it to "index" template
+    con.query('SELECT * FROM auction', function(err, rows){
+        if(err) throw err;
+
+        res.render('index', {
+            title: 'Big Data Auction House',
+            rows: rows
+        });
+    });
 });
 
 module.exports = router;
