923c2830ab44d3e69e5607c6701a40c197010510
Brian Yang <brian@brianyang.me>
2015-11-28 02:05:42 -0500

    Implement creating item and auction listings

--- routes/auction.js
+++ routes/auction.js
@@ -36,7 +36,8 @@ router.get('/:id', function (req, res) {
 
             res.render('auction', {
                 title: 'Viewing Auction ' + req.params.id,
-                auction: rows[0] // should only return one row
+                auction: rows[0], // should only return one row
+                post: req.query.post ? true : false // show the new auction success alert
             });
         }
     );
@@ -53,13 +54,66 @@ router.get('/:id', function (req, res) {
  *    - Item Quantity
  *    - Minimum Bid
  *    - Bid Increment
+ *
  */
 router.post('/new', function (req, res) {
-    res.send(req.body.id);
+    // first add the item
+    db.query('INSERT INTO item (Description, Name, Type, NumCopies)' +
+        'VALUES (?, ?, ?, ?)', [req.body.description, req.body.name, req.body.type, req.body.copies],
+        function (err, rows) {
 
+            // TEMPORARY HARDCODED EMPLOYEE AND CUSTOMER FOR NOW
+            req.body.monitor = 1;
+            req.body.customer = 222;
 
+            // to group:
+            // this callback function is executed when the query is complete,
+            // and so on for the ones below
 
+            // item ID is auto-incremented. Select LAST_INSERT_ID gets that ID
+            // might run into concurrency issues but its fine for this project
+            db.query('SELECT LAST_INSERT_ID()', function (err, rows) {
 
+                // id of newly inserted item
+                var id = rows[0]['LAST_INSERT_ID()'];
+
+                // add auction of that item
+                db.query('INSERT INTO auction (BidIncrement, MinimumBid, CopiesSold, Monitor, ItemID)' +
+                    'VALUES (?, ?, ?, ?, ?)',
+                    [req.body.increment, req.body.minimum, req.body.copies, req.body.monitor, id],
+                    function (err, rows) {
+
+                        // I don't like how he made us organize this database...
+                        db.query('SELECT LAST_INSERT_ID()', function (err, rows) {
+
+                            // same deal as above but with new auction
+                            id = rows[0]['LAST_INSERT_ID()'];
+
+                            // insert auction into the post table, indicating who is posting the auction
+                            db.query('INSERT INTO post VALUES (?, ?, ?, NOW(), ?)',
+                                [req.body.customer, id, req.body.expire, req.body.reserve],
+                                function (err, rows) {
+                                    var data = {};
+
+                                    if (err) {
+                                        console.log("ERROR - Error inserting auction");
+                                        data.success = false;
+
+                                    } else {
+                                        data.success = true;
+                                        data.id = id; // send the id so the page can redirect
+                                    }
+
+                                    res.setHeader('content-type', 'application/json');
+                                    return res.send(JSON.stringify(data));
+                                }
+                            ); // end insert post query
+                        }); // end get auction ID query
+                    }
+                ); // end insert auction query
+            }); // end get item ID query
+        }
+    ); // end insert item query
 });
 
 module.exports = router;
